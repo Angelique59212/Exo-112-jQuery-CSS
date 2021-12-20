66bed5cfd15322ba9bf73eb592403b066d237477
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-20 14:35:34 +0100

    css jquery

---
+++ exercice3/exercice.js
@@ -0,0 +1,2 @@
+let para = $("p").css('background-color');
+alert(para);
---
+++ exercice4/exercice.js
@@ -0,0 +1,8 @@
+let para = $("p").css({
+    "backgroundColor":"green",
+    "color" : "white",
+    "font-size" : "25px",
+    "padding" : "15px",
+});
+
+
